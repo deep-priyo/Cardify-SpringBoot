b5c62c8e07bd773ce5633810bf98075add78b929
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2025-03-20 21:16:11 +0530

    Add user creation and retrieval endpoints

---
+++ src/main/java/io/aiven/spring/mysql/spring_app/MainController.java
@@ -0,0 +1,23 @@
+package io.aiven.spring.mysql.spring_app;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/user")
+public class MainController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody User addNewUser(@RequestParam String name, @RequestParam String email) {
+        User springUser = new User();
+        springUser.setName(name);
+        springUser.setEmail(email);
+        userRepository.save(springUser);
+        return springUser;
+    }
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
