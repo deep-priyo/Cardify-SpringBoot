909b0c8f0b4d3363f3774c4b741837def0856b7e
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2025-03-20 21:15:57 +0530

    Add Spring Boot application entry point

---
+++ src/main/java/io/aiven/spring/mysql/spring_app/SpringAppApplication.java
@@ -0,0 +1,14 @@
+package io.aiven.spring.mysql.spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringAppApplication.class, args);
+	}
+
+}
