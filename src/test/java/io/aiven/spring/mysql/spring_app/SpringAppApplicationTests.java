f4b5d927c066cce8ecaaa11dbe6ef462f4c00ede
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2025-03-20 21:19:05 +0530

    Added basic unit test for Spring Boot context loading

---
+++ src/test/java/io/aiven/spring/mysql/spring_app/SpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aiven.spring.mysql.spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
